53b5a4db0db25cb6c2c639850ffbea677cd8591a
Ravi Kumar <1002kumarravi@gmail.com>
2021-09-01 12:19:52 +0530

    feat-blank-input

--- index.js
+++ index.js
@@ -3,41 +3,49 @@ const dateOfBirth = document.querySelector("#date-of-birth")
 const luckyNumber = document.querySelector("#lucky-number")
 const checkBtn = document.querySelector("#check-btn")
 const displayMsg = document.querySelector("#display-msg")
+const formEl = document.querySelector("#my-form")
+const contentBox = document.querySelector(".content")
 
 
 
 
-
-function userOutputValue(){
+function userOutputValue() {
     let dobInput = dateOfBirth.value
     let luckyNumberInput = Number(luckyNumber.value)
-    if( dobInput && luckyNumberInput){
-     let birthDateVaule= dobNumberAdd(dobInput)
-  
-     if(birthDateVaule%luckyNumberInput===0){
-         displayMsg.innerText = "you are lucky"
-     }else{
-         displayMsg.innerText = "you are unlucky"
-     }
-    } else{
-        displayMsg.innerText = "please fill both input"
+    if (dobInput && luckyNumberInput) {
+        let birthDateVaule = dobNumberAdd(dobInput)
+
+        if (birthDateVaule % luckyNumberInput === 0) {
+            displayMsg.innerText = "you are lucky"
+            contentBox.style.padding = "1.9rem 3rem"
+        } else {
+            displayMsg.innerText = "you are unlucky"
+            contentBox.style.padding = "1.9rem 3rem"
+        }
     }
-    }
+}
 
 
-  
- function dobNumberAdd(addDobInput){
-    let dobHyphenClear =  addDobInput.replaceAll("-", "") 
+
+function dobNumberAdd(addDobInput) {
+    let dobHyphenClear = addDobInput.replaceAll("-", "")
     let sum = 0
-    
-    for(i =0; i < addDobInput.length; i++){
+
+    for (i = 0; i < addDobInput.length; i++) {
         sum += Number(dobHyphenClear.charAt(i))
     }
-   
-   return sum
 
- }
+    return sum
 
-checkBtn.addEventListener("click", function(){
-  userOutputValue()
+}
+
+formEl.addEventListener("submit", function (e) {
+
+    e.preventDefault()
+})
+
+
+checkBtn.addEventListener("click", function () {
+
+    userOutputValue()
 })
